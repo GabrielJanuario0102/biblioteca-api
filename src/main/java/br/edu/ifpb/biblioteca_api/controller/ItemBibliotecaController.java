33176d87c92da0cc125d7f1a56f1c1ce040c89a6
GabrielJanuario0102 <gabriel.januario@academico.ifpb.edu.br>
2026-08-06 20:04:49 -0300

    feat: adição do controller para itemBiblioteca

---
+++ src/main/java/br/edu/ifpb/biblioteca_api/controller/ItemBibliotecaController.java
@@ -0,0 +1,61 @@
+package br.edu.ifpb.biblioteca_api.controller;
+
+import java.util.List;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import br.edu.ifpb.biblioteca_api.model.ItemBiblioteca;
+import br.edu.ifpb.biblioteca_api.service.ItemBibliotecaService;
+import jakarta.validation.Valid;
+
+@RestController
+@RequestMapping("/itens")
+public class ItemBibliotecaController {
+
+    private final ItemBibliotecaService service;
+
+    public ItemBibliotecaController(ItemBibliotecaService service) {
+        this.service = service;
+    }
+
+    @GetMapping
+    public ResponseEntity<List<ItemBiblioteca>> listarTodos() {
+        return ResponseEntity.ok(service.listarTodos());
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<ItemBiblioteca> buscarPorId(@PathVariable String id) {
+        return ResponseEntity.ok(service.buscarPorId(id));
+    }
+
+    @PostMapping
+    public ResponseEntity<ItemBiblioteca> salvar(
+            @Valid @RequestBody ItemBiblioteca itemBiblioteca) {
+
+        ItemBiblioteca itemSalvo = service.salvar(itemBiblioteca);
+
+        return ResponseEntity.status(HttpStatus.CREATED)
+                .body(itemSalvo);
+    }
+
+    @PutMapping("/{id}")
+    public ResponseEntity<ItemBiblioteca> atualizar(
+            @PathVariable String id,
+            @Valid @RequestBody ItemBiblioteca itemBiblioteca) {
+
+        ItemBiblioteca itemAtualizado = service.atualizar(id, itemBiblioteca);
+
+        return ResponseEntity.ok(itemAtualizado);
+    }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<Void> deletar(@PathVariable String id) {
+
+        service.deletar(id);
+
+        return ResponseEntity.noContent().build();
+    }
+
+}
